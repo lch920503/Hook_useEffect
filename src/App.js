3542b19119d0b790104ea9417c927e2acded8b4f
lch09253 <lch09253@gmail.com>
2023-10-30 22:41:58 +0900

    feat: useEffect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
-function App() {
+const App = () => {
+  const [sum, setSum] = useState(0);
+
+  const handleAdd = () => {
+    setSum(sum + 1);
+  };
+
+  useEffect(() => {
+    console.log("나는 마운트 될 때 실행됩니다");
+  });
+
+  useEffect(() => {
+    console.log("나는 렌더링 될 때 한번만 실행됩니다");
+  }, []);
+
+  useEffect(() => {
+    console.log("나는 sum이 변할 때만 실행됩니다");
+  }, [sum]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>UseEffect</h1>
+      <div>
+        값 증가: {sum}
+        <button onClick={handleAdd}>1씩 더하기</button>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
